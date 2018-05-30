20134048e25d07dba1631e665ac51823eae71d39
lucong <332553882@qq.com>
2018-05-30 19:31:10 +0800

    vue hello

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 
-import {createApp} from './app'
+// import {createApp} from './app'
 
-const { app } = createApp({})
+// const { app } = createApp({})
 
-app.$mount('#app')
+// app.$mount('#app')
 
+import Vue from 'vue'
+import App from './app.vue'
+
+let createApp = (context = {}) =>{
+  return new Vue({
+    el : '#app',
+    data: context,
+    render: h => h(App)
+  })
+}
+
+createApp()
